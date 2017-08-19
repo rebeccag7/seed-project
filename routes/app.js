db6c59d40bce7d4fcc6e3ddd8ee193354a2a20a2
Rebecca Goveia <rebeccagoveia@gmail.com>
2017-08-18 22:41:23 -0700

    Added models and tested saving to and retrieving from the database

--- routes/app.js
+++ routes/app.js
@@ -1,17 +1,35 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/user');
 
 router.get('/', function (req, res, next) {
-    res.render('index');
+	User.findOne({}, function(err, doc) {
+		if(err) {
+			return res.send('Error!');
+		}
+		res.render('node', {email: doc.email});
+	});
 });
 
-router.get('/message/:msg', function (req, res, next) {
+router.post('/', function(req, res, next) {
+	var email = req.body.email;
+	var user = new User({
+		firstName: 'Rebecca',
+		lastName: 'Goveia',
+		password: 'spider',
+		email: email
+	});
+	user.save();
+	res.redirect('/');
+});
+
+/*router.get('/message/:msg', function (req, res, next) {
     res.render('node', {message: req.params.msg});
 });
-
 router.post('/message', function (req, res, next) {
     var message = req.body.message;
     res.redirect('/message/' + message);
 });
+*/
 
 module.exports = router;
