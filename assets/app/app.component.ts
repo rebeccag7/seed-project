dc148ddb559b9a9ff11a0c47c6945dfaf7b10d0a
Rebecca Goveia <rebeccagoveia@gmail.com>
2017-08-28 16:12:39 -0700

    Worked with event binding

--- assets/app/app.component.ts
+++ assets/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 
+import { Message } from "./messages/message.model";
+
 @Component({
     selector: 'my-app',
     templateUrl: './app.component.html',
 })
-export class AppComponent {
+/*export class AppComponent {
     message = {
     	content: 'A message',
         author: 'Rebecca'
     };
+}*/
+
+export class AppComponent {
+    message: Message = new Message('Some message', 'Rebecca');
 }
--- assets/app/messages/message.component.ts
+++ assets/app/messages/message.component.ts
@@ -1,4 +1,6 @@
-import { Component } from "@angular/core";
+import { Component, Input, Output, EventEmitter } from "@angular/core";
+
+import { Message } from "./message.model";
 
 @Component({
 	selector: 'app-message',
@@ -19,5 +21,11 @@ import { Component } from "@angular/core";
     `]
 })
 export class MessageComponent {
+	@Input() message: Message;
+	@Output() editClicked = new EventEmitter<string>();
 
+	onEdit() {
+		alert('it worked');
+		this.editClicked.emit('A new value');
+	}
 }
